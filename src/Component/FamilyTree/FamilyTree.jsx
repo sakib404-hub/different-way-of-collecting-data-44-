ba2de5bdab35d8d2462db9c55c301da991a1a5e3
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-08 00:46:24 +0600

    context api means withour prop drilling

--- src/Component/FamilyTree/FamilyTree.jsx
+++ src/Component/FamilyTree/FamilyTree.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Grandpa from './Grandpa';
+
+export const AssetContext = createContext('');
 
 const FamilyTree = () => {
     const asset = 'Diamond'
+    const asset2 = 'Black Diamond'
     return (
         <div>
             <h2 className='text-2xl font-bold'>Family Tree</h2>
-            <Grandpa asset={asset}></Grandpa>
+            <AssetContext.Provider value={asset2}>
+                <Grandpa asset={asset}></Grandpa>
+            </AssetContext.Provider>
         </div>
     );
 };
--- src/Component/FamilyTree/Special.jsx
+++ src/Component/FamilyTree/Special.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AssetContext } from './FamilyTree';
 
 const Special = ({ asset, anotherAsset }) => {
+    const newAsset = useContext(AssetContext);
     return (
         <div className='border border-cyan-300 m-4 p-4 rounded-2xl'>
             <h3>Special : {asset} {anotherAsset}</h3>
+            <p>New Asset :{newAsset}</p>
         </div>
     );
 };
